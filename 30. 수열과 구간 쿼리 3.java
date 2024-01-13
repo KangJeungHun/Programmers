2a9d1c6fc0a47914d4666a8b7b337aa1fa54a784
KangJeungHun <128660252+KangJeungHun@users.noreply.github.com>
2024-01-13 19:27:41 +0900

    Update 30. 수열과 구간 쿼리 3.java

--- 30. 수열과 구간 쿼리 3.java
+++ 30. 수열과 구간 쿼리 3.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         for(int i = 0; i<queries.length; i++){
-            int index0 = queries[i][0];
-            int index1 = queries[i][1];
-            int temp = arr[index0];
+            int index0 = queries[i][0]; // 바꿔질 arr의 index
+            int index1 = queries[i][1]; // 바꿀 arr의 index
+            int temp = arr[index0]; // 원래 값
             
-            arr[index0] = arr[index1];
+            arr[index0] = arr[index1]; 
             arr[index1] = temp;
         }
         return arr;

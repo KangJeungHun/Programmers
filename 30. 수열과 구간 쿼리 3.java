72d1267df30bd0cf459c96e5476f523d1c69235a
KangJeungHun <128660252+KangJeungHun@users.noreply.github.com>
2024-01-13 19:25:53 +0900

    Create 30. 수열과 구간 쿼리 3.java

---
+++ 30. 수열과 구간 쿼리 3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for(int i = 0; i<queries.length; i++){
+            int index0 = queries[i][0];
+            int index1 = queries[i][1];
+            int temp = arr[index0];
+            
+            arr[index0] = arr[index1];
+            arr[index1] = temp;
+        }
+        return arr;
+    }
+}
